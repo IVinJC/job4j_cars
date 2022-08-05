aac0d5fb9aa23f7083aba5849c45b6fb01e1a6b0
Windows <ivinjc@gmail.com>
2022-08-05 20:20:09 +0300

    Модели и связи. Машины и владельцы [#4744]

---
+++ src/main/java/ru/job4j/Driver.java
@@ -0,0 +1,44 @@
+package ru.job4j;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "driver")
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "history_owner",
+            joinColumns = {
+                    @JoinColumn(name = "driver_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {
+                    @JoinColumn(name = "car_id", nullable = false, updatable = false)})
+    private List<Car> carList = new ArrayList<>();
+
+    public Driver() {
+    }
+
+    public Driver(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/job4j/Engine.java
@@ -0,0 +1,45 @@
+package ru.job4j;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "engine")
+public class Engine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    public Engine() {
+    }
+
+    public Engine(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Engine{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
+
